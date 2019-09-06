c320a2be1cb9102abca38d22de45bd8e81edc20c
Le Quang <lequang.it99@gmail.com>
2019-09-06 22:22:33 +0700

    Done 4

---
+++ Bai 4/SinhVien.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_4
+{
+    class SinhVien
+    {
+        private string name;
+        private string tenMonHoc;
+        private int soTinChi;
+        private int diem;
+
+        public SinhVien(string name, string tenMonHoc, int soTinChi, int diem)
+        {
+            this.Name = name;
+            this.TenMonHoc = tenMonHoc;
+            this.SoTinChi = soTinChi;
+            this.Diem = diem;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string TenMonHoc { get => tenMonHoc; set => tenMonHoc = value; }
+        public int SoTinChi { get => soTinChi; set => soTinChi = value; }
+        public int Diem { get => diem; set => diem = value; }
+    }
+}
---
+++ Bai 4/frmMain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai_4
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CbName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbName.SelectedIndex != -1)
+            {
+                if (cbName.SelectedIndex == 0)
+                {
+                    txtSoTinChi.Text = "2";
+                }
+                else if (cbName.SelectedIndex == 1)
+                {
+                    txtSoTinChi.Text = "3";
+                }
+                else if (cbName.SelectedIndex == 2)
+                {
+                    txtSoTinChi.Text = "3";
+                }
+                else if (cbName.SelectedIndex == 3)
+                {
+                    txtSoTinChi.Text = "2";
+                }
+                else if (cbName.SelectedIndex == 4)
+                {
+                    txtSoTinChi.Text = "3";
+                }
+            }
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            Exit();
+        }
+        private void AddList()
+        {
+
+        }
+        private void Exit()
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning)
+                == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == (Keys.Alt | Keys.H))
+            {
+                Exit();
+            }
+            if (keyData == (Keys.Alt | Keys.D))
+            {
+                AddList();
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+    }
+}
